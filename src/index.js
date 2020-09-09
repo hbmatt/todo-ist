a4af0e60a34413e09de9b9066a989489ae4376c4
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-09-09 17:06:36 -0500

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ const closeTask = document.getElementById('closetask');
 closeTask.addEventListener('click', () => {
   taskForm.classList.add('hide');
 });
+
+const newProject = document.getElementById('newproject');
+const projectForm = document.getElementById('projectform');
+newProject.addEventListener('click', () => {
+  projectForm.classList.remove('hide');
+});
+const closeProject = document.getElementById('closeproject');
+closeProject.addEventListener('click', () => {
+  projectForm.classList.add('hide');
+});
